f3b0b03dfe67c6b350ddcde8e8c14bdc993ec8c6
조주영 <jooyoung@finda.co.kr>
2023-05-11 14:36:40 +0900

    chore: lint import rule 추가 및 react-query 설치

---
+++ src/app/ReactQueryProvider.tsx
@@ -0,0 +1,14 @@
+import { PropsWithChildren, useState } from 'react';
+import { QueryClient, QueryClientProvider, Hydrate } from 'react-query';
+import { ReactQueryDevtools } from 'react-query-devtools';
+
+export default function ReactQueryProvider({ children }: PropsWithChildren) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
