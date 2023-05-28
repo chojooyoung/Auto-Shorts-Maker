310ba5a3907bd0d358848a520fc3dae44fe6db7e
조주영 <jooyoung@finda.co.kr>
2023-05-29 02:09:19 +0900

    feat:구글연동

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,14 +2,26 @@
 
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import useGoogleAuth from '@/hooks/useGoogleAuth';
+import useAuthStore from '@/store/authStore';
 
 function Header() {
   const router = useRouter();
+  const { handleOAuthLoginButtonClick } = useGoogleAuth();
 
+  const [accessToken] = useAuthStore((s) => [
+    s.accessToken,
+    s.deleteAccessToken,
+    s.setAccessToken,
+  ]);
   const handleBannerClick = () => {
     router.push('/');
   };
 
+  const isLogin = accessToken?.length !== 0;
+
+  console.log(accessToken);
+  console.log(isLogin);
   return (
     <header className="flex justify-between">
       <button type="button" onClick={handleBannerClick}>
@@ -17,7 +29,9 @@ function Header() {
       </button>
       <div className="flex gap-2">
         <Link href="/myPage">My Page</Link>
-        <button type="button">Login</button>
+        <button type="button" onClick={handleOAuthLoginButtonClick}>
+          {isLogin ? 'LogOut' : 'Login'}
+        </button>
       </div>
     </header>
   );
---
+++ src/hooks/useGoogleAuth.ts
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import useAuthStore from '@/store/authStore';
+import useStorage from './useStorage';
+
+const useGoogleAuth = () => {
+  const GOOGLE_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
+
+  const GoogleURL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_ID}&response_type=token&redirect_uri=http://localhost:3000&scope=https://www.googleapis.com/auth/userinfo.email`;
+
+  const [accessToken, setAccessToken, deleteAccessToken] = useAuthStore((s) => [
+    s.accessToken,
+    s.setAccessToken,
+    s.deleteAccessToken,
+  ]);
+
+  const { setItem, removeItem } = useStorage();
+
+  const isLogin = accessToken;
+
+  const handleOAuthLoginButtonClick = () => {
+    if (isLogin) {
+      deleteAccessToken();
+      removeItem('accessToken', 'session');
+      return;
+    }
+    window.location.assign(GoogleURL);
+  };
+
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const { hash } = url;
+    if (hash) {
+      const token = hash.split('=')[1].split('&')[0];
+      setItem('accessToken', token, 'session');
+      setAccessToken(token);
+    }
+  }, []);
+
+  return {
+    handleOAuthLoginButtonClick,
+  };
+};
+
+export default useGoogleAuth;
