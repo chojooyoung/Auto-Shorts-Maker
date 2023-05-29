6f7a92ab0a1471cab32ed96744a2f2dd07c6be71
조주영 <jooyoung@finda.co.kr>
2023-05-29 21:59:48 +0900

    fix: 강제 router 라우팅

--- src/hooks/useGoogleAuth.ts
+++ src/hooks/useGoogleAuth.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import useAuthStore from '@/store/authStore';
 import useStorage from './useStorage';
 
@@ -7,6 +8,8 @@ const useGoogleAuth = () => {
   const GOOGLE_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
 
   const GoogleURL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_ID}&response_type=token&redirect_uri=http://localhost:3000&scope=https://www.googleapis.com/auth/userinfo.email`;
+
+  const router = useRouter();
 
   const [accessToken, setAccessToken, deleteAccessToken] = useAuthStore((s) => [
     s.accessToken,
@@ -34,6 +37,7 @@ const useGoogleAuth = () => {
       const token = hash.split('=')[1].split('&')[0];
       setItem('accessToken', token, 'session');
       setAccessToken(token);
+      router.push('/');
     }
   }, []);
 
