67ebe6c3636a67c1fda5a245cf6c4c9687cb2e78
조주영 <jooyoung@finda.co.kr>
2023-05-30 10:23:01 +0900

    fix:로그인 버그 해결

--- src/hooks/useGoogleAuth.ts
+++ src/hooks/useGoogleAuth.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import useAuthStore from '@/store/authStore';
 import useStorage from './useStorage';
@@ -17,7 +17,7 @@ const useGoogleAuth = () => {
     s.deleteAccessToken,
   ]);
 
-  const { setItem, removeItem } = useStorage();
+  const { setItem, removeItem, getItem } = useStorage();
 
   const isLogin = accessToken;
 
@@ -27,17 +27,21 @@ const useGoogleAuth = () => {
       removeItem('accessToken', 'session');
       return;
     }
+    const url = window.location.pathname;
+    setItem('beforeUrl', url, 'session');
     window.location.assign(GoogleURL);
   };
 
   useEffect(() => {
     const url = new URL(window.location.href);
     const { hash } = url;
-    if (hash) {
+    if (hash && hash.includes('access_token')) {
+      const beforeLoginUrl = getItem('beforeUrl', 'session');
       const token = hash.split('=')[1].split('&')[0];
+
       setItem('accessToken', token, 'session');
       setAccessToken(token);
-      router.push('/');
+      router.replace(beforeLoginUrl);
     }
   }, []);
 
